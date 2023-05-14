664c0966e3d5c87cb6a9997a767077e5cc23deba
문정우 <106897607+MoonjeongWoo@users.noreply.github.com>
2023-05-14 15:38:45 +0900

    practice flutter
    parctice flutter 1

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
         return MaterialApp(
             home: Scaffold(
-              appBar: AppBar(),
-              body: Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceEvenly, //가로축
-               // crossAxisAlignment: CrossAxisAlignment.center, //세로축
-              ),
-              bottomNavigationBar: BottomAppBar(
+              appBar: AppBar(title: Text("내가만든 어플")),
+              body: Align(
+                alignment: Alignment.center,
                 child: Container(
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(Icons.phone),
-                      Icon(Icons.message),
-                      Icon(Icons.contact_page)
-                    ],
-                  ),
+                  width: 50, height: 50, color: Colors.amber,
+                  // margin: EdgeInsets.all(20), 전체 마진 주기
+                  // padding: EdgeInsets.all(5), 전체 패딩 주기
+                  // margin: EdgeInsets.fromLTRB(100, 50, 100, 50), //마진 시계방향으로 주기
+                  // padding: EdgeInsets.fromLTRB(10, 10, 10, 10), //  패딩 시계방향으로 주기
+                  // decoration: BoxDecoration( // 얘는 박스 데코레이션 해주는 애임
+                  //   border: Border.all(color: Colors.black), // 테두리 넣기
+                  //
+                  // ),
+                  child: Text("동희형 바봉 "),
                 ),
               )
-          )
+            ),
+
         );
   }
 }
